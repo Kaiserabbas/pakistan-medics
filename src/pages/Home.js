1539dd380dda062507e208a3679822488b800657
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2024-12-02 10:19:34 +0500

    Add page home

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Welcome to Pakistan Medics</h1>
+    <p>Your welfare in the medical field for disaster management.</p>
+  </div>
+);
+
+export default Home;
