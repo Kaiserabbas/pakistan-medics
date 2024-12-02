9c1fe88d7ee917dc1e09f6a316ba77178a45adc3
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2024-12-02 10:18:26 +0500

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#75C049', color: 'white', textAlign: 'center', padding: '10px',
+    }}
+    >
+      <p>&copy; 2024 Pakistan Medics. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
